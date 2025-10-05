e8d8e8db58d7bacbcf889b1d381186bb20634f39
unknown <arsmoroz@ya.ru>
2025-10-05 15:33:07 +0700

    Hello WebGPU!

--- client.cpp
+++ client.cpp
@@ -6,8 +6,86 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <webgpu/webgpu.h>
+#include <webgpu/webgpu_cpp.h>
+
 #include <string>
 #include <format>
+#include <iostream>
+
+wgpu::Instance instance;
+wgpu::Adapter adapter;
+wgpu::Device device;
+wgpu::RenderPipeline pipeline;
+
+wgpu::Surface surface;
+wgpu::TextureFormat format;
+const uint32_t kWidth = 512;
+const uint32_t kHeight = 512;
+
+const char shaderCode[] = R"(
+    @vertex fn vertexMain(@builtin(vertex_index) i : u32) ->
+      @builtin(position) vec4f {
+        const pos = array(vec2f(0, 1), vec2f(-1, -1), vec2f(1, -1));
+        return vec4f(pos[i], 0, 1);
+    }
+    @fragment fn fragmentMain() -> @location(0) vec4f {
+        return vec4f(0, 1, 0, 1);
+    }
+)";
+
+void ConfigureSurface() {
+  wgpu::SurfaceCapabilities capabilities;
+  surface.GetCapabilities(adapter, &capabilities);
+  format = capabilities.formats[0];
+
+  wgpu::SurfaceConfiguration config{.device = device,
+                                    .format = format,
+                                    .width = kWidth,
+                                    .height = kHeight,
+                                    .presentMode = wgpu::PresentMode::Fifo};
+  surface.Configure(&config);
+}
+
+void CreateRenderPipeline() {
+  wgpu::ShaderSourceWGSL wgsl{{.code = shaderCode}};
+
+  wgpu::ShaderModuleDescriptor shaderModuleDescriptor{.nextInChain = &wgsl};
+  wgpu::ShaderModule shaderModule = device.CreateShaderModule(&shaderModuleDescriptor);
+
+  wgpu::ColorTargetState colorTargetState{.format = format};
+
+  wgpu::FragmentState fragmentState{
+      .module = shaderModule, .targetCount = 1, .targets = &colorTargetState};
+
+  wgpu::RenderPipelineDescriptor descriptor{.vertex = {.module = shaderModule},
+                                            .fragment = &fragmentState};
+  pipeline = device.CreateRenderPipeline(&descriptor);
+}
+
+void Render() {
+  wgpu::SurfaceTexture surfaceTexture;
+  surface.GetCurrentTexture(&surfaceTexture);
+
+  wgpu::RenderPassColorAttachment attachment{.view = surfaceTexture.texture.CreateView(),
+                                             .loadOp = wgpu::LoadOp::Clear,
+                                             .storeOp = wgpu::StoreOp::Store};
+
+  wgpu::RenderPassDescriptor renderpass{.colorAttachmentCount = 1, .colorAttachments = &attachment};
+
+  wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+  wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
+  pass.SetPipeline(pipeline);
+  pass.Draw(3);
+  pass.End();
+  wgpu::CommandBuffer commands = encoder.Finish();
+  device.GetQueue().Submit(1, &commands);
+}
+
+void InitGraphics() {
+  ConfigureSurface();
+  CreateRenderPipeline();
+}
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -53,34 +131,60 @@ EM_BOOL onmessage(int eventType, const EmscriptenWebSocketMessageEvent *socketEv
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   SDL_SetAppMetadata("Surfive", "1.0", "");
 
+  static const auto kTimedWaitAny = wgpu::InstanceFeatureName::TimedWaitAny;
+  wgpu::InstanceDescriptor instanceDesc{.requiredFeatureCount = 1,
+                                        .requiredFeatures = &kTimedWaitAny};
+
+  instance = wgpu::CreateInstance(&instanceDesc);
+
+  wgpu::Future f1 = instance.RequestAdapter(
+      nullptr, wgpu::CallbackMode::WaitAnyOnly,
+      [](wgpu::RequestAdapterStatus status, wgpu::Adapter a, wgpu::StringView message) {
+        if (status != wgpu::RequestAdapterStatus::Success) {
+          std::cout << "RequestAdapter: " << message.data << "\n";
+          exit(0);
+        }
+        adapter = std::move(a);
+      });
+  instance.WaitAny(f1, UINT64_MAX);
+
+  wgpu::DeviceDescriptor desc{};
+  desc.SetUncapturedErrorCallback(
+      [](const wgpu::Device &, wgpu::ErrorType errorType, wgpu::StringView message) {
+        std::cout << "Error: " << (int)errorType << " - message: " << message.data << "\n";
+      });
+
+  wgpu::Future f2 = adapter.RequestDevice(
+      &desc, wgpu::CallbackMode::WaitAnyOnly,
+      [](wgpu::RequestDeviceStatus status, wgpu::Device d, wgpu::StringView message) {
+        if (status != wgpu::RequestDeviceStatus::Success) {
+          std::cout << "RequestDevice: " << message.data << "\n";
+          exit(0);
+        }
+        device = std::move(d);
+      });
+  instance.WaitAny(f2, UINT64_MAX);
+
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
-  if (!SDL_CreateWindowAndRenderer("surfive", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
-    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-    return SDL_APP_FAILURE;
-  }
+  SDL_Window *window = SDL_CreateWindow("Learn WebGPU", 640, 480, 0);
 
-  std::string bmp_path = std::format("{}sample.bmp", SDL_GetBasePath());
-  SurfaceBMP surface(bmp_path.c_str());
+  wgpu::EmscriptenSurfaceSourceCanvasHTMLSelector fromCanvasHTMLSelector;
+  fromCanvasHTMLSelector.sType = wgpu::SType::EmscriptenSurfaceSourceCanvasHTMLSelector;
+  fromCanvasHTMLSelector.nextInChain = NULL;
+  fromCanvasHTMLSelector.selector = WGPUStringView{.data = "canvas", .length = 6};
 
-  /* Textures are pixel data that we upload to the video hardware for fast drawing. Lots of 2D
-     engines refer to these as "sprites." We'll do a static texture (upload once, draw many
-     times) with data from a bitmap file. */
+  wgpu::SurfaceDescriptor surfaceDescriptor = {};
+  surfaceDescriptor.label = "HTML5 surface";
+  surfaceDescriptor.nextInChain = &fromCanvasHTMLSelector;
 
-  /* SDL_Texture is pixel data the GPU can access.
-     Load a .bmp into a surface, move it to a texture from there. */
+  surface = instance.CreateSurface(&surfaceDescriptor);
+  SDL_Log("surface = %p", (void *)&surface);
 
-  texture_width = surface.get()->w;
-  texture_height = surface.get()->h;
-
-  texture = SDL_CreateTextureFromSurface(renderer, surface.get());
-  if (!texture) {
-    SDL_Log("Couldn't create static texture: %s", SDL_GetError());
-    return SDL_APP_FAILURE;
-  }
+  InitGraphics();
 
   my_id = std::rand() + std::rand();
   SDL_Log("my_id: %d\n", my_id);
@@ -129,62 +233,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
   g_timers.framePerf.tick(SDL_GetTicks());
-
-  const float x0 = 0.5f * WINDOW_WIDTH;
-  const float y0 = 0.5f * WINDOW_HEIGHT;
-  const float px = SDL_min(WINDOW_WIDTH, WINDOW_HEIGHT) / SDL_sqrtf(3.0f);
-
-  const float rad = (((float)((int)((now + 2000) % 2000))) / 2000.0f) * SDL_PI_F * 2;
-  const float cos = SDL_cosf(rad);
-  const float sin = SDL_sinf(rad);
-  const float k[3] = {3.0f / SDL_sqrtf(50.0f), 4.0f / SDL_sqrtf(50.0f), 5.0f / SDL_sqrtf(50.0f)};
-  float mat[9] = {
-      cos + (1.0f - cos) * k[0] * k[0],         -sin * k[2] + (1.0f - cos) * k[0] * k[1],
-      sin * k[1] + (1.0f - cos) * k[0] * k[2],  sin * k[2] + (1.0f - cos) * k[0] * k[1],
-      cos + (1.0f - cos) * k[1] * k[1],         -sin * k[0] + (1.0f - cos) * k[1] * k[2],
-      -sin * k[1] + (1.0f - cos) * k[0] * k[2], sin * k[0] + (1.0f - cos) * k[1] * k[2],
-      cos + (1.0f - cos) * k[2] * k[2],
-  };
-
-  float corners[16];
-  int i;
-
-  for (i = 0; i < 8; i++) {
-    const float x = (i & 1) ? -0.5f : 0.5f;
-    const float y = (i & 2) ? -0.5f : 0.5f;
-    const float z = (i & 4) ? -0.5f : 0.5f;
-    corners[0 + 2 * i] = mat[0] * x + mat[1] * y + mat[2] * z;
-    corners[1 + 2 * i] = mat[3] * x + mat[4] * y + mat[5] * z;
-  }
-
-  SDL_SetRenderDrawColor(renderer, 0x42, 0x87, 0xf5, SDL_ALPHA_OPAQUE); // light blue background.
-  SDL_RenderClear(renderer);
-
-  for (i = 1; i < 7; i++) {
-    const int dir = 3 & ((i & 4) ? ~i : i);
-    const int odd = (i & 1) ^ ((i & 2) >> 1) ^ ((i & 4) >> 2);
-    if (0 < (odd ? 1.0f : -1.0f) * mat[5 + dir])
-      continue;
-    int origin_index = (1 << ((dir - 1) % 3));
-    int right_index = (1 << ((dir + odd) % 3)) | origin_index;
-    int down_index = (1 << ((dir + (odd ^ 1)) % 3)) | origin_index;
-    if (!odd) {
-      origin_index ^= 7;
-      right_index ^= 7;
-      down_index ^= 7;
-    }
-    SDL_FPoint origin, right, down;
-    origin.x = x0 + px * corners[0 + 2 * origin_index];
-    origin.y = y0 + px * corners[1 + 2 * origin_index];
-    right.x = x0 + px * corners[0 + 2 * right_index];
-    right.y = y0 + px * corners[1 + 2 * right_index];
-    down.x = x0 + px * corners[0 + 2 * down_index];
-    down.y = y0 + px * corners[1 + 2 * down_index];
-    SDL_RenderTextureAffine(renderer, texture, NULL, &origin, &right, &down);
-  }
-
-  SDL_RenderPresent(renderer);
-
+  Render();
   return SDL_APP_CONTINUE;
 }
 
